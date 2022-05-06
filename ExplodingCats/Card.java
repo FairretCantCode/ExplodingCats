fe8a2da009cd2a232853fa466444d8b842175ef6
toxip <toxip@Buch.fios-router.home>
2022-05-06 15:40:58 -0400

    first commit

---
+++ ExplodingCats/Card.java
@@ -0,0 +1,16 @@
+public class Card{
+
+  private String name;
+  public Card(String n){
+    name = n;
+  }
+
+  public String getName(){
+    return name;
+  }
+  
+  public void effect(){
+    
+  }
+  
+}
