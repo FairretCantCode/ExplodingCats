6f636c5f859c09c0131c3e396e54c79fd4696712
FairretCantCode <63377164+FairretCantCode@users.noreply.github.com>
2022-05-11 09:21:22 -0400

    Dumb

--- ExplodingCats/client/GUI.java
+++ ExplodingCats/client/GUI.java
@@ -1,15 +1,39 @@
 import javax.swing.*;
-
-public class GUI {
+import java.awt.event.*;
+public class GUI implements ActionListener{
 	
+	
+	JFrame frame = new JFrame("Exploding Daub");
+	
+	public GUI 
 	public void startApplication() {
-		JFrame frame = new JFrame("Exploding Daub");
+		
 		frame.setVisible(true);
 		frame.setSize(800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+
 		JButton startButton = new JButton("Start Game");
+		{}
 		frame.getContentPane().add(startButton);
+		startButton.addActionListener(this);
+		
+		
+	}
+	public void gameApp() {
+		JFrame game = new JFrame("Exploding Daub");
+		game.setVisible(true);
+		game.setSize(1600,1200);
+		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	
 	}
 	
 }
--- ExplodingCats/client/User.java
+++ ExplodingCats/client/User.java
@@ -8,13 +8,15 @@ public class User {
 	public User() {
 		gui = new GUI();
 		int PORT = 4999;
-		String ip = "localhost";
+		String ip = "192.168.1.131";
 		client = new Client(PORT, ip);
 		
 	}
 	
 	public void startGame() {
 		client.startConnection();
+		
 		gui.startApplication();
+		client.run();
 	}
 }
